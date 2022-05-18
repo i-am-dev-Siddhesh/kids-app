3bd9c54978d4be77a40db82bdb4dabe4f59414de
i-am-dev-Siddhesh <shindesiddhesh.co.in@gmail.com>
2022-05-18 22:43:49 +0530

    fix: bubbles added in banner part

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,24 +1,37 @@
+import { Bubble } from "../../svgs/Bubble";
 import styles from "./banner.module.css";
 
 export const Banner = () => {
+    const arr = ["flock", "herd", "circle", "pile", "clump", "army"];
+
+    const onClick = (text) => {
+        console.log("text", text);
+    };
     return (
         <div className={styles.bannerParent}>
-            <div className={styles.firstSection}>
-                <div
-                    style={{
-                        width: "100%",
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    <button className={styles.questionNumber}>
-                        <p>Question 4/10</p>
-                    </button>
-                    <div className={styles.questionBox}>
-                        <p>King Midas was a very rich man because he had ...</p>
-                    </div>
-                </div>
+            <div
+                style={{
+                    width: "60%",
+                    margin: "auto",
+                    display: "flex",
+                    alignItems: "center",
+                    flexWrap: "wrap",
+                }}
+            >
+                {arr.map((item, index) => {
+                    return (
+                        <span
+                            key={index}
+                            style={{
+                                margin: "auto",
+                                position: "relative",
+                                top: index % 2 === 0 ? "-70px" : "0px",
+                            }}
+                        >
+                            <Bubble text={item} onClick={onClick} />
+                        </span>
+                    );
+                })}
             </div>
             <div className={styles.secondSection}></div>
         </div>
--- src/svgs/Bubble.js
+++ src/svgs/Bubble.js
@@ -1,20 +1,35 @@
-import React from "react";
-
-export const Bubble = () => {
+export const Bubble = ({ text, onClick }) => {
     return (
         <svg
-            width="58"
-            height="58"
-            viewBox="0 0 58 58"
+            width="118"
+            height="118"
+            viewBox="0 0 78 78"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
         >
-            <circle cx="29" cy="29" r="29" fill="white" fill-opacity="0.5" />
+            <circle
+                cx="29"
+                cy="29"
+                r="29"
+                fill="white"
+                fillOpacity="0.5"
+                style={{
+                    cursor: "pointer",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            />
             <path
                 d="M13 11.5C15 8.49997 21.5 1.49997 36 4.49997C52.5 11.4999 51.5 23 50.5 24C49.5 25 47.5 27.5 43.5 22C39.5 16.5 35.5 15 31.5 14C27.5 13 23 15 18.5 17C14 19 11 14.5 13 11.5Z"
                 fill="white"
                 fillOpacity="0.5"
             />
+            <a onClick={() => onClick(text)}>
+                <text x="12" y="34" fill="white">
+                    {text}
+                </text>
+            </a>
         </svg>
     );
 };
