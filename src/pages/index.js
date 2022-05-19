61d4aa3f2e1617ad0cb379024cd583f14ed14906
i-am-dev-Siddhesh <shindesiddhesh.co.in@gmail.com>
2022-05-19 11:11:44 +0530

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
                 <style>
                     @import
-                    url('https://fonts.googleapis.com/css2?family=Fredoka:wght@600&family=Hurricane&display=swap');
+                    url('https://fonts.googleapis.com/css2?family=Fredoka:wght@600`&rsquo;`family=Hurricane`&rsquo;`display=swap');
                 </style>
             </Head>
 
